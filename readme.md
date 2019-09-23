b82516b34303934564abf7f03f13800bb1486aa0
Yaoyi6 <luoyaoyi@outlook.com>
2019-09-23 11:06:00 +0200

    init projet

---
+++ readme.md
@@ -0,0 +1,2 @@
+# the worsrt game ever
+run it with `npx serve` then get on http://localhost:5000
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: './src/index.html',
+        to: path.resolve(__dirname, 'dist')
+      },
+    ]),
+  ]
+};
